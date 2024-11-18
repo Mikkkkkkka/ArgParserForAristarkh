bf15179107958c288efb1b1e892b3043d3129935
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2024-11-18 03:54:27 +0300

    Initial Commit

---
+++ argparser.h
@@ -0,0 +1,26 @@
+﻿//
+// Created by NeSai on 16/11/2024.
+//
+
+#ifndef ARGPARSER_H
+#define ARGPARSER_H
+
+#include <vector>
+#include <string>
+#include <map>
+
+class argparser {
+
+    std::map<std::string, std::string> params;  // Дикшионари хранящий пары ключ значение
+
+public:
+    bool Parse(std::vector<std::string> args);      // Парсим аргументы
+    int GetIntegerValue(std::string key);           // Возвращаем значение параметра интом
+    bool GetBoolValue(std::string key);             // Возвращаем значение параметра булом
+    std::string GetStringValue(std::string key);    // Возвращаем значение параметра строкой
+    void PrintAll();
+};
+
+
+
+#endif //ARGPARSER_H
---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "argparser.h"
+
+// Пример програмных аргументов
+// --name=Alexander --age=17 -- single=true
+
+int main(int argc, char** argv) {
+
+    // Собираем аргументы в вектор
+    std::vector<std::string> args;
+    for (int i = 0; i < argc; i++)
+        args.emplace_back(argv[i]);
+
+    argparser parser;
+    parser.Parse(args);
+
+    // Пользуемся классом
+    std::cout << "Hi " << parser.GetStringValue("name") << "!" << std::endl;
+    std::cout << "You're " << parser.GetIntegerValue("age") << ", ";
+    std::cout << "and " << (parser.GetBoolValue("single") ? "still don't " : "already ") << "have a girlfriend?" << std::endl;
+
+
+    std::cout << std::endl;
+    parser.PrintAll();
+
+    return 0;
+}
